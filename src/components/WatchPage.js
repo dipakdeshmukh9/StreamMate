12b5b57e26b64e3f04910db64288963980b21ec7
Dipak Deshmukh <dipak.deshmukh1911@gmail.com>
2024-07-25 19:31:47 +0530

    live chat

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,8 +2,8 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useSearchParams } from "react-router-dom";
 import { closeMenu } from "../utils/appSlice";
-import CommentsContainer from "./CommentsContainer";
 import LiveChat from "./LiveChat";
+import CommentsContainer from "CommentsContainer.js";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
